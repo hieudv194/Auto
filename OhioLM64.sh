9de58b1a28ec2d9d39bc3f780f88b50e95090613
hieudv194 <hieudv194@gmail.com>
2025-03-17 22:20:27 +0700

    Update OhioLM64.sh

--- OhioLM64.sh
+++ OhioLM64.sh
@@ -34,7 +34,7 @@ for region in "${!region_image_map[@]}"; do
     image_id=${region_image_map[$region]}
 
     # Kiểm tra Key Pair
-    key_name="LM64KeyPair-$region"
+    key_name="DHKeyPair-$region"
     if aws ec2 describe-key-pairs --key-names "$key_name" --region "$region" > /dev/null 2>&1; then
         echo "Key Pair $key_name đã tồn tại trong $region"
     else
