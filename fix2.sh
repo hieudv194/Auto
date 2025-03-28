faf87c2aef0c97e1c177513ea65a52978abee09a
hieudv194 <hieudv194@gmail.com>
2025-03-28 13:00:14 +0700

    Update and rename fix1.sh to fix2.sh

--- fix2.sh
+++ fix2.sh
@@ -24,7 +24,7 @@ echo "Creating Subnet..."
 SUBNET_ID=$(aws ec2 create-subnet \
   --vpc-id $VPC_ID \
   --cidr-block 10.0.1.0/24 \
-  --availability-zone "${REGION}a" \
+  --availability-zone "us-east-1a" \
   --query 'Subnet.SubnetId' \
   --output text)
 
